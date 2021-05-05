f4276d5ee0cbe3ad6864aff0c0409c4a519abe34
Lucas Silva <lucas.silva@codeminer42.com>
2021-05-04 21:36:55 -0300

    adding cors in the api

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import createConnection from '@shared/infra/typeorm';
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import errorHandler from '@shared/errors/errorHandler';
 import routes from './routes';
@@ -10,6 +11,8 @@ import routes from './routes';
 import '@shared/container';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
