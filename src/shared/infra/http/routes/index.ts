8b7473b439ce1b31aa40e7bc1ac8c0c13ae9e9af
Lucas Silva <lucas.silva@codeminer42.com>
2021-05-20 16:30:20 -0300

    feature: forgot password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import bondsRouter from '@modules/users/infra/http/routes/bonds.routes';
 import coursesRouter from '@modules/users/infra/http/routes/courses.routes';
-import rolesRouter from '@modules/users/infra/http/routes/roles.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
@@ -11,16 +10,18 @@ import equipmentsRouter from '@modules/rentable-items/infra/http/routes/equipmen
 import roomsRouter from '@modules/rentable-items/infra/http/routes/rooms.routes';
 import sportCourtsRouter from '@modules/rentable-items/infra/http/routes/sportCourts.routes';
 
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/bonds', ensureAuthenticated, bondsRouter);
 routes.use('/courses', ensureAuthenticated, coursesRouter);
-routes.use('/roles', ensureAuthenticated, rolesRouter);
 
 routes.use('/equipments', ensureAuthenticated, equipmentsRouter);
 routes.use('/rooms', ensureAuthenticated, roomsRouter);
 routes.use('/sportCourts', ensureAuthenticated, sportCourtsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
